41a053c10b58c8c8ed796756a365dbe85fc596c4
Bran Liang <lby89757@hotmai.com>
2023-09-24 19:00:36 +0800

    init

---
+++ lib/sha256_length_extension_attack/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Sha256LengthExtensionAttack
+  VERSION = "0.1.0"
+end
---
+++ test/test_sha256_length_extension_attack.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require "test_helper"
+
+class TestSha256LengthExtensionAttack < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::Sha256LengthExtensionAttack::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
